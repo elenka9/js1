bf1b4b3ba796b8292b82fc9d7125b7024360c9f4
Elena_Andrianova <elena_andrianova10@mail.ru>
2024-09-02 16:08:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var greeting = "Hello world!";
+console.log(greeting);
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(sum);
+
+let name = "Alice";
+name = "Bob"; //почему зачеркнуто name?
+console.log(name);
+
+var str = "Hello";
+var number = 42;
+var bulevo = true;
+console.log(typeof str, number, true);
+
+let age = 25;
+let str1 = String(age);
+console.log(str1);
+
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + lastName;
+console.log(fullName);
+
+let strNumber = "100";
+let number1 = Number(strNumber);
+console.log(number1);
+
+var score = 95;
+var str2 = String(score);
+console.log(str2);
+
+let isLoggedIn = true;
+let str3 = isLoggedIn.toString();
+console.log(str3);
